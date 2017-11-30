0365b1252da2ddc50e21e06a3efe945d96c48376
jairOrjuela <genius85862@gmail.com>
2017-11-30 11:48:45 -0500

    ozzu app v-1

---
+++ db/migrate/20171130015302_create_dogs.rb
@@ -0,0 +1,11 @@
+class CreateDogs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.integer :age
+      t.belongs_to :owner, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
